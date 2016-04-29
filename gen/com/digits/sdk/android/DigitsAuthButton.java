7519dd04ddeeab24f3e7985c7743c2b6c5ea7ac7
Gabriel Lando <glando.py@gmail.com>
2016-04-30 00:10:00 +0300

    Commit stuff

---
+++ gen/com/digits/sdk/android/DigitsAuthButton.java
@@ -0,0 +1,14 @@
+package com.digits.sdk.android;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by LandoKe on 4/29/2016.
+ */
+public class DigitsAuthButton extends View {
+    public DigitsAuthButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
